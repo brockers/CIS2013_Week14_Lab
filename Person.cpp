a9e652e211a672c88ae8fd3585f752fbed7c253e
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-04-24 19:18:31 -0500

    Person and card classes created and compiling

---
+++ Person.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include "Card.cpp"
+
+using namespace std;
+
+class Person {
+
+	private:
+	
+		string name;
+		bool is_hitting;
+		Card hand[9];
+	
+	public:
+	
+		// Constructor
+		Person(){
+			set_name("unknown");
+			set_hitting(true);
+		}
+		
+		void set_name(string n){ name = n; }
+		void set_hitting(bool h){ is_hitting = h; }
+};
